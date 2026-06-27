551d0ee3e7c6eea68a23d691fa41b9fbabb590b3
feuzavalol <bilal.navelot@gmail.com>
2026-06-27 02:02:49 +0200

    Add pages for Users, Food and Transport, as well as Dashboard with basic CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Vite from "./Vite"
+import Dashboard from './layouts/Dashboard'
+// import Vite from "./Vite"
 import Users from "./pages/Users"
+import Food from './pages/Food'
+import Transport from './pages/Transport'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Vite />} >
+        <Route path="/" element={<Dashboard />}>
+          {/* <Route index element={<Vite />} /> */}
+          <Route path="/users" element={<Users />} />
+          <Route path="/food" element={<Food />} />
+          <Route path="/transport" element={<Transport />} />
         </Route>
-        <Route path="/users" element={<Users />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/layouts/Dashboard.tsx
@@ -0,0 +1,33 @@
+import { NavLink, Outlet } from "react-router-dom";
+import "./Dashboard.css"
+
+function Dashboard() {
+  return (
+    <>
+      <div className="dashboard">
+        <nav className="navbar">
+          <h1>Outil carbone des Mines</h1>
+          <ul>
+            <li><NavLink to="/">Accueil</NavLink></li>
+            <li><NavLink to="/users">Utilisateurs</NavLink></li>
+            <li><NavLink to="/food">Alimentation</NavLink></li>
+            <li><NavLink to="/transport">Transport</NavLink></li>
+            <li><NavLink to="/goods">Biens</NavLink></li>
+          </ul>
+        </nav>
+        
+        <main className="content">
+          <Outlet /> {/* Les pages s'affichent ici */}
+        </main>
+      </div>
+      <footer>
+        <p>
+          Cet outil n'a pas de vocation à être utilisé dans un contexte professionel ou sérieux. <br />
+          Il est a utilisé avec du recul et un minimum de connaissance sur le sujet.
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default Dashboard;
